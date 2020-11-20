79bdc96f7577a6c3a168f75241846b93b4639db2
Ikaia Leleiwi <ileleiwi@gmail.com>
2020-11-20 09:30:33 -0700

    added .csv to end of dates df

--- data_cleaning.R
+++ data_cleaning.R
@@ -78,5 +78,5 @@ master_covid_election_with_dates <- covid_counties_clean_dates %>%
 #Writing master dataframes to data folder
 
 write_csv(master_covid_election, "./data/master_covid_election.csv")
-write_csv(master_covid_election_with_dates, "./data/master_covid_election_with_dates")
+write_csv(master_covid_election_with_dates, "./data/master_covid_election_with_dates.csv")
 
