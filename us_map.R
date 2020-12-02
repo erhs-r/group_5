d636ddc4f598640b2b9924ba743e43de5b9baab2
Ky Tanner <ky.tanner@colostate.edu>
2020-12-02 11:46:13 -0700

    Convert US Map to leflet map

--- us_map.R
+++ us_map.R
@@ -1,14 +1,12 @@
-library(scales)
 library(tidyverse)
-library(purrr)
 library(tigris)
-library(viridis)
+library(leaflet)
 
 source("data_cleaning.R")
 
-counties <- counties(state = c("WA", "OR", "CA", "NV", "AZ", "NM", "CO", "MN", "WI", 
+counties <- counties(state = c("WA", "OR", "CA", "NV", "AZ", "NM", "CO", "MN", "WI", "AK", 
                                "IL", "MI", "GA", "PA", "VA", "NY", "VT", "NH", "MA", "RI", 
-                               "CT", "NJ", "DE", "MD", "ID", "UT", "MT", "WY", "ME",
+                               "CT", "NJ", "DE", "MD", "ID", "UT", "MT", "WY", "ME", "HI",
                                "ND", "SD", "KS", "OK", "TX", "LA", "IA", "MO", "AR", "MS",
                                "AL", "TN", "KY", "IN", "OH", "WV", "NC", "SC", "FL", "NE"),
                      cb = TRUE, class = "sf") %>% 
@@ -16,13 +14,19 @@ counties <- counties(state = c("WA", "OR", "CA", "NV", "AZ", "NM", "CO", "MN", "
   select(fips, geometry)
 
 counties_data <- counties %>% 
-  inner_join(master_covid_election, by = "fips")
+  inner_join(master_covid_election, by = "fips") %>% 
+  mutate(winner = str_to_title(winner),
+         popup_text = paste("<b>State:</b>", str_to_title(state), "<br>",
+                            "<b>County:</b>", str_to_title(county), "<br>"))
+
+pal <- colorFactor(c("#2E3FD5", "#E7191C"), counties_data$winner)
+
+leaflet() %>% 
+  setView(lng = -110, lat = 40, zoom = 3) %>% 
+  addTiles() %>% 
+  addPolygons(data = counties_data, color = "#000000", weight = 0.5,
+              fillColor = ~ pal(winner), popup = ~ popup_text) %>% 
+  addLegend(data = counties_data, pal = pal, values = ~ winner,
+            title = "Winner")
   
-ggplot() +
-  geom_sf(data = counties_data, aes(fill = winner,
-                                    )) +
-  scale_fill_discrete(type = c("blue", "red")) +
-  theme(legend.position = "bottom") +
-  theme(axis.line = element_blank(),
-        axis.text = element_blank(),
-        axis.ticks = element_blank())
+
