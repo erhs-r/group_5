97ca8d920d99b0687752ed08a713fde831d13228
Linda Kim <linda.kim@colostate.edu>
2020-12-08 18:43:44 -0700

    Chart 3 and changed the dashboard format

---
+++ mask_final.R
@@ -0,0 +1,68 @@
+source("data_cleaning.R")
+library(ggplot2)
+library(ggthemes)
+library(tidyverse)
+library(ggbeeswarm)
+library (plotly)
+
+#calculate case_rate per state 
+masks <- master_covid_election %>% 
+  mutate(state_infection_rate = (total_cases/ state_population)*100000, 
+         total_mask = (frequently + always)*100)
+#find top 3 states
+#north dakota, south dakota, iowa
+worst_case_state <- masks %>% 
+  group_by(state) %>%
+  select(state,state_infection_rate) %>% 
+  unique() %>% 
+  ungroup() %>% 
+  top_n(3, wt = state_infection_rate) %>% 
+  pull(state)
+
+#cleaned data for creating plot
+state_mask <- masks %>%
+  subset(state %in% worst_case_state) %>% 
+  mutate (state = str_to_title(state), 
+          winner = str_to_title(winner),
+          county = str_to_title(county))
+
+med <- state_mask %>% 
+  group_by(state, winner) %>% 
+  summarize( median_mask = median(total_mask)) %>% 
+  ungroup %>% 
+  inner_join(state_mask, by = c("state", "winner"))
+   
+
+#plot ggplot   
+state_plot <- med%>% 
+  ggplot(aes(x = winner, y = total_mask, 
+             text = paste("County:", county, "<br>",
+                        "Case per 100k:", round(infection_rate_per_100k),"<br>",
+                        "Median:", median_mask))) +
+  geom_quasirandom(aes(color = winner, size = infection_rate_per_100k), 
+                   alpha=.5, show.legend = FALSE)+
+  facet_wrap(.~ fct_reorder(state, state_infection_rate, .fun = max, 
+                            .desc = TRUE), ncol = 3) + 
+  scale_y_continuous(labels=function(total_mask) paste0(total_mask,"%"))+
+  scale_color_manual(values = c("blue", "red"))+
+  labs(x = "", y = "", size ="infection") +
+  scale_x_discrete(position ="top") +
+  theme_bw()
+
+
+
+
+mask_plot<- ggplotly(state_plot, 
+                     tooltip = "text",
+                     texttemplate = "hello",
+                     textposition = "inside") %>% 
+  layout(yaxis = list(titlefont = list(size = 15), title =
+                        "% of people who frequently or\n always wear their mask"
+                      , automargin = T))
+      
+
+
+
+#final plotlk
+mask_plot
+
